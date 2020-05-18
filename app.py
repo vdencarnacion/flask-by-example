0a814aeebcd3d55d8a604f74e221e7d2ab9142b1
vdencarnacion <vdencarnacion@gmail.com>
2020-05-18 23:35:05 +1000

    count words

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
 import requests
+import operator
+import re
+import nltk
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -21,11 +27,43 @@ def index():
         try:
             url = request.form['url']
             r = requests.get(url)
-            print(r.text)
+            # print(r.text)
         except:
             errors.append(
                 "Unable to get URL. Please make sure it's valid and try again"
             )
+            return render_template('index.html', errors=errors)
+        if r:
+            print(r)
+            # text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            # set the path
+            nltk.data.path.append('./nltk_data/')
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[:10]
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append('Unable to add item to database')
     return render_template('index.html', errors=errors, results=results)
 
 
