e6ef268bf82b18e54af7a38a86ab9b7505abb4ac
vdencarnacion <vdencarnacion@gmail.com>
2020-06-03 23:38:42 +1000

    http post works, and poller

--- static/main.js
+++ static/main.js
@@ -11,25 +11,53 @@
     }
   ]);*/
 
-  .controller('WordcountController', ['$scope', '$log', '$http',
-    function($scope, $log, $http) {
+  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
       $scope.getResults = function() {
-
         $log.log("test");
 
         // get the URL from the input
         var userInput = $scope.url;
+        // $log.log('userInput');
+        // $log.log(userInput);
 
         // fire the API request
         $http.post('/start', {"url": userInput}).
           success(function(results) {
+            // $log.log('success-start');
             $log.log(results);
+            getWordCount(results);
+
           }).
           error(function(error) {
             $log.log(error);
           });
       };
+
+      function getWordCount(jobID) {
+          var timeout = "";
+
+          var poller = function() {
+            // fire another request
+            $http.get('/results/'+jobID).
+              success(function(data, status, headers, config) {
+                if(status === 202) {
+                  $log.log(data, status);
+                } else if (status === 200){
+                  $log.log(data);
+                  $scope.wordcounts = data;
+                  $timeout.cancel(timeout);
+                  return false;
+                }
+                // continue to call the poller() function every 2 seconds
+                // until the timeout is cancelled
+                timeout = $timeout(poller, 2000);
+              });
+          };
+          poller();
+        };
     }
+    
   ]);
 
 }());
